a008d54bdbbf2a5c5dd1ae2c8c8fce8b69e32fa2
NihalSh <nihal.sh.1995@gmail.com>
2018-01-07 15:33:06 +0530

    startup and setup scripts modified for auth and account service

--- startup.sh
+++ startup.sh
@@ -4,3 +4,7 @@ sudo docker service rm itemservice
 sudo docker service create --name=itemservice --replicas=1 --network=my_network -p=6767:6767 itemservice
 sudo docker service rm orderservice
 sudo docker service create --name=orderservice --replicas=1 --network=my_network -p=4242:4242 orderservice
+sudo docker service rm auth-service 
+sudo docker service create --name=auth-service --replicas=1 --network=my_network -p=60000:60000 auth-service
+sudo docker service rm account-service
+sudo docker service create --name=account-service --replicas=1 --network=my_network -p=60001:60001 account-service
