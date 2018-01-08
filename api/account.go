de403b2aed00d1752006ab054520902e8a68b722
niksoc <nikhil_2811@hotmail.com>
2018-01-08 22:56:43 +0530

    changed accountservice to account-service

--- api/account.go
+++ api/account.go
@@ -9,9 +9,9 @@ import (
 
 func ApproveOrder(userId string, orderId string, amount int) error {
 	params := url.Values{"order_id": {orderId}, "amount": {fmt.Sprintf("%v", amount)}, "userid": {userId}}
-	resp, err := http.PostForm("http://accountservice:60001/approveorder", params)
+	resp, err := http.PostForm("http://account-service:60001/approveorder", params)
 	if err != nil {
-		return fmt.Errorf("POST http://accountservice:60001/ params = %v: %v", params, err)
+		return fmt.Errorf("POST http://account-service:60001/ params = %v: %v", params, err)
 	}
 	if resp.StatusCode != 200 && resp.StatusCode != 201 {
 		return errors.New(resp.Status)
