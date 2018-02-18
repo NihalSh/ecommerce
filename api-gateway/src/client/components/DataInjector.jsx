6a8f51879139176596c7d332db5a19541efaaddd
niksoc <nikhil_2811@hotmail.com>
2018-02-18 18:27:53 +0530

    added DataInjector component that fetches data and injects it into its child

---
+++ api-gateway/src/client/components/DataInjector.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class DataInjector extends React.Component {
+
+    init(props) {
+	this.setState({
+	    data: null
+	});
+	this.fetchData(props.url);
+    }
+
+    constructor(props) {
+	super(props);
+	this.state = {
+	    data: null
+	};
+    }
+
+    componentDidMount() {
+	this.init(this.props);
+    }
+
+    componentWillReceiveProps(nextProps) {
+	this.init(nextProps);
+    }
+
+    fetchData(url) {
+	// TODO: replace with api call
+	if (this.props.url == 'productsUrl') {
+	    this.setState({
+		data: [
+		    {
+			id: '20ln12n',
+			name: 'shampoo',
+			description: 'cleans your hair',
+			image:'/250x250.png',
+		    },
+		    {
+			id: 'o3unf2o',
+			name: 'toothpaste',
+			description: 'cleans your teeth',
+			image:'/250x250.png',
+		    },
+		],
+	    });
+	} else {
+	    console.error("unsupported url");
+	}
+    }
+
+    render() {
+	return this.state.data === null? this.props.loadingIndicator : this.props.children(this.state.data);
+    }
+    
+
+}
+
+DataInjector.propTypes = {
+    url: PropTypes.string,
+    loadingIndicator: PropTypes.func,
+};
+
+export default DataInjector;
