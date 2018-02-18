1e13ca685f16ba73ff59c327b5c6109ecb97dad6
niksoc <nikhil_2811@hotmail.com>
2018-02-18 19:45:18 +0530

    added reviews data in DataInjector, loading indicator is made optional

--- api-gateway/src/client/components/DataInjector.jsx
+++ api-gateway/src/client/components/DataInjector.jsx
@@ -27,30 +27,72 @@ class DataInjector extends React.Component {
 
     fetchData(url) {
 	// TODO: replace with api call
-	if (this.props.url == 'products') {
+	let shampoo = {
+	    id: '20ln12n',
+	    name: 'shampoo',
+	    description: 'cleans your hair',
+	    image:'/250x250.png',
+	};
+	let toothpaste = {
+	    id: 'o3unf2o',
+	    name: 'toothpaste',
+	    description: 'cleans your teeth',
+	    image:'/250x250.png',
+	};
+
+	if (url == 'products') {
 	    this.setState({
-		data: [
-		    {
-			id: '20ln12n',
-			name: 'shampoo',
-			description: 'cleans your hair',
-			image:'/250x250.png',
-		    },
-		    {
-			id: 'o3unf2o',
-			name: 'toothpaste',
-			description: 'cleans your teeth',
-			image:'/250x250.png',
-		    },
-		],
+		data: [shampoo, toothpaste],
 	    });
+	} else if (url.startsWith('products')) {
+	    let id = url.split('/')[1];
+	    switch(id) {
+	    case shampoo.id: this.setState({data: shampoo}); break;
+	    case toothpaste.id: this.setState({data: toothpaste}); break;
+	    }
+	} else if (url.startsWith('reviews')) {
+	    let id = url.split('/')[1];
+	    switch(id) {
+	    case shampoo.id: this.state.data = [
+		{
+		    id: 'o3pnyult',
+		    item_id: shampoo.id,
+		    author: '3n09i3ue',
+		    title: 'Very disappointed',
+		    rating: 1,
+		    text: 'Does not work on dogs',
+		},
+		{
+		    id: 'o32nniet',
+		    item_id: shampoo.id,
+		    author: '33yun3ue',
+		    title: 'Gives shiny hair!',
+		    rating: 4,
+		    text: 'Shiny shiny!',
+		},
+	    ];
+		break;
+	    case toothpaste.id: this.state.data = [
+		{
+		    id: 'o3pnyult',
+		    item_id:toothpaste.id,
+		    author: '3n09i3ue',
+		    title: 'Not excellent not bad',
+		    rating: 3,
+		    text: 'not mediocre not outstanding, only cleans teeth nothing else',
+		},
+	    ];
+		break;
+	    }
+	    this.setState({data: this.state.data});
 	} else {
 	    console.error("unsupported url");
 	}
     }
 
     render() {
-	return this.state.data === null? this.props.loadingIndicator : this.props.children(this.state.data);
+	return this.state.data === null?
+	    (this.props.loadingIndicator? this.props.loadingIndicator : null) : this.props.children(this.state.data);
     }
     
 
