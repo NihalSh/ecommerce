3f34eda534f7912fd91d51f35c3d9a3934a4287f
Nihal Shriwastawa <NihalSh@users.noreply.github.com>
2018-03-11 20:37:06 +0530

    feat: add redux-logger to support logging

--- api-gateway/src/client/configureStore.js
+++ api-gateway/src/client/configureStore.js
@@ -1,15 +1,21 @@
 import {
+  applyMiddleware,
   createStore,
   combineReducers
 } from 'redux';
+import {
+  createLogger
+} from 'redux-logger';
 import rootReducer from './reducers';
+
+const logger = createLogger();
 
 export default function configureStore() {
   if (process.env.NODE_ENV !== 'production' && window.store) {
     window.store.replaceReducer(rootReducer);
     return window.store;
   }
-  const store = createStore(rootReducer);
+  const store = createStore(rootReducer, applyMiddleware(logger));
   if (process.env.NODE_ENV !== 'production') {
     window.store = store;
   }
