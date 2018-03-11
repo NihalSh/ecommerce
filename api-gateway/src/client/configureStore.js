2dc5570877487540db83f082e95948b1481e5f60
Nihal Shriwastawa <NihalSh@users.noreply.github.com>
2018-03-11 18:00:30 +0530

    feat: update reducer on change

--- api-gateway/src/client/configureStore.js
+++ api-gateway/src/client/configureStore.js
@@ -6,6 +6,7 @@ import rootReducer from './reducers';
 
 export default function configureStore() {
   if (process.env.NODE_ENV !== 'production' && window.store) {
+    window.store.replaceReducer(rootReducer);
     return window.store;
   }
   const store = createStore(rootReducer);
